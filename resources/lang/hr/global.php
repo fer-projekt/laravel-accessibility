6330a4718cd2a706b1b42077c804cf6bece78951
Matej Hanzlić <31820060+Hanzlo@users.noreply.github.com>
2024-11-28 10:56:10 +0100

    Create global.php hr

---
+++ resources/lang/hr/global.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'accessibility-menu'                    => 'Pristupačnost',
+    'monochrome'                            => 'Jednobojna slika',
+    'dark-contrast'                         => 'Tamni kontrast',
+    'bright-contrast'                       => 'Svijetli kontrast',
+    'decrease-font-size'                    => 'Smanji veličinu fonta',
+    'increase-font-size'                    => 'Povećaj veličinu fonta',
+    'font-family'                           => 'Obitelj fontova',
+    'cursor-bw'                             => 'Veliki bijeli kursor',
+    'cursor-bb'                             => 'Veliki crni kursor',
+    'zoom'                                  => 'Povećanje',
+    'highlight-links'                       => 'Istakni poveznice',
+    'highlight-titles'                      => 'Istakni naslove',
+    'alt-description'                       => 'Dodatni Opis',
+    'reset'                                 => 'Resetiraj',
+    'disable-transitions'                   => 'Onemogući prijelaze',
+    'quick-navigation'                      => 'Izbornik za navigaciju na tipkovnici',
+    'info-enable-quick-navigation'          => 'Uvijek možete pritisnuti (:a) za povratak na brzu navigaciju',
+    'press-to-open-menu'                    => 'Pritisnite (:a) Da otvorite izbornik pristupačnosti',
+    'press-to-enable-quick-navigation'      => 'Pritisnite (:a) Za omogućavanje izbornika za navigaciju na tipkovnici'
+];
